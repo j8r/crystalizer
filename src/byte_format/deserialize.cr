40977617f1016bafe84e4fdf9bf6ec34c0712993
Davor Ocelic <docelic@crystallabs.io>
2021-09-01 16:55:44 +0200

    Update src/byte_format/deserialize.cr
    Co-authored-by: Julien Reichardt <git@jrei.ch>

--- src/byte_format/deserialize.cr
+++ src/byte_format/deserialize.cr
@@ -88,7 +88,7 @@ struct Crystalizer::ByteFormat
   end
 
   # :ditto:
-  def deserialize(to type : String.class, size : Range)
+  def deserialize(to type : String.class, size : Range(Int, Int))
     string = if max_size = size.end
                # An alternative to reading limit `max_size + 1` would be to read `max_size` or `max_size - 1`
                # (depending on `size.excludes_end?`), then peek the next char for '\0' and either consume it
